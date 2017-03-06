e2ab74dcd5de1860de28373a032e3fc3e3d0e8ec
Felipe Cerignoni <felipe@imaflora.org>
2017-03-06 16:42:00 -0300

    Added registration form

--- app/containers/Footer.js
+++ app/containers/Footer.js
@@ -1,6 +1,15 @@
-import { Button } from 'react-bootstrap'
+import { Button, FormGroup, FormControl, ControlLabel  } from 'react-bootstrap'
 import Collapse from '../components/Collapse'
 import MyModal from '../components/MyModal'
+
+function FieldGroup({ id, label, ...props }) {
+          return (
+            <FormGroup controlId={id}>
+              <ControlLabel>{label}</ControlLabel>
+              <FormControl {...props} />
+            </FormGroup>
+          );
+        }
 
 export class Footer extends React.Component {
     constructor(props) {
@@ -10,7 +19,11 @@ export class Footer extends React.Component {
         }
     }
 
+    
+    
     render() {
+        
+        
         return (
             <div className="footer">
                 <Button onClick={ ()=> this.setState({ open: !this.state.open })} style={{float:"none", marginLeft: "auto"}}>Show footer</Button>
@@ -23,7 +36,43 @@ export class Footer extends React.Component {
                             <td className="align-center">Atlas: a geografia da agropecuária brasileira</td>
                             <td className="align-right footer-right">
                                 <MyModal show={this.state.showModal} onHide={() => this.setState({showModal: false})} title="Example title">
-                                    Contents
+                                    <form>
+                                        <FieldGroup 
+                                            id="email"
+                                            type="email"
+                                            label="Endereço de E-mail"
+                                            placeholder="exemplo@exemplo.com"    
+                                        />
+                                        <FieldGroup 
+                                            id="name"
+                                            type="text"
+                                            label="Nome do usuário"
+                                            placeholder="Insira o nome"    
+                                        />
+                                        <FieldGroup 
+                                            id="inst"
+                                            type="text"
+                                            label="Instituição"
+                                            placeholder="Insira a instituição"    
+                                        />
+                                        <FieldGroup 
+                                            id="dep"
+                                            type="text"
+                                            label="Departamento"
+                                            placeholder="Insira o departamento"    
+                                        />
+                                        <FieldGroup 
+                                            id="tel"
+                                            type="tel"
+                                            label="Telefone para contato"
+                                            placeholder="ex. 55(00)90000-0000"    
+                                        />
+                                        <FormGroup controlId="finalidade">
+                                            <ControlLabel>Finalidade de Uso</ControlLabel>
+                                            <FormControl componentClass="textarea" placeholder="Área do texto" />
+                                        </FormGroup>
+
+                                    </form>
                                 </MyModal>
                                 <Button id="botao" onClick={() => this.setState({showModal: true})}> Teste </ Button>
                             </td>
