122b9c7a68c3de5f040140e69d1c66658b9a814b
Felipe Cerignoni <felipe@imaflora.org>
2017-03-13 17:15:57 -0300

    styles updated

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,6 @@ module.exports = {
     new webpack.ProvidePlugin({
         "classNames": "classnames"}),
     new ExtractTextPlugin("[name].css"),
-      new ExtractTextPlugin("header.css"),
     new webpack.DefinePlugin({
       'process.env':{
         'NODE_ENV': process.env.NODE_ENV ? JSON.stringify(process.env.NODE_ENV) : JSON.stringify('DEBUG')
