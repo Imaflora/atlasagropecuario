fb358d73c9adec57886d323dbec8535fbcbb889e
Felipe Cerignoni <fcerignoni@gmail.com>
2017-03-15 01:05:26 -0300

    Update responsive style

--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -12,16 +12,14 @@ export class MainContainer extends React.Component {
 		return (
 			// Main div block with gray background				
 			<div>
-              <div>
-                <div className="row">
-                  <div>
-                  <img id="atlas_logo" src={require("../img/atlas_logo.png")} alt="Logo do Atlas da Agropecuária Brasielira"/>
+              <div className="row">
+                  <div  id="atlas_logo" className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                  <img src={require("../img/atlas_logo.png")} alt="Logo do Atlas da Agropecuária Brasielira"/>
                   </div>
-                  <div id="realiz_logo">
+                  <div id="realiz_logo" className="hidden-xs col-sm-6 col-md-6 col-lg-6">
                   <img src={require("../img/imaflora_logo.png")} alt="Logo do Imaflora"/>
                   <img src={require("../img/esalq_logo.png")} alt="Logo da Esalq"/>
                   </div>    
-                </div>
                 <div className="HEADER_CINZA"></div>
                 <div className="HEADER_BRANCO"></div>
               </div>
