bd00974589ce522cae94f8eabb63aa4f959395ce
Caio Hamamura <caio@imaflora.org>
2017-03-17 16:41:38 -0300

    Removed es2015 notation again

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.options('/*', (req, res, next) => {
+app.options('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
