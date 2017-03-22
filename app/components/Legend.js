727534d16e5eebdfa357afc1112f4e25a1c37667
Felipe Cerignoni <felipe@imaflora.org>
2017-03-22 09:07:18 -0300

    Fixed legend label

--- app/components/Legend.js
+++ app/components/Legend.js
@@ -43,7 +43,7 @@ class Legend extends React.Component {
             },
             {
                 color: '#74a9cf',
-                label: 'Quilombolas',
+                label: 'Territórios Comunitários',
             },
             {
                 color: '#252525',
