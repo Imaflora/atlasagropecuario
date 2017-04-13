5526663cf0340749c99385ba0dc6e6b7c6b3f286
Caio Hamamura <caio@imaflora.org>
2017-04-13 12:08:49 -0300

    Metadata downloads pdf directly

--- app/containers/Metadata.js
+++ app/containers/Metadata.js
@@ -15,7 +15,7 @@ const Metadata = props => {
             noClose>
             {props.metadata}
             <br />
-            <a className="downloadMetadata" href={props.link} target="_blank">{props.translation['downloadMetadata']}</a>
+            <a className="downloadMetadata" href={props.link}>{props.translation['downloadMetadata']}</a>
         </MyModal>
     )
 }
