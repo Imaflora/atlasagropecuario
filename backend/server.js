4f4c85fc94e92c16bcdad868143b65685d665e09
Caio Hamamura <caio@imaflora.org>
2017-05-24 16:46:21 -0300

    Added possibility to use production database for testing purposes defining NODE_SERVER environmental variable

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ var config = {
   user: 'postgres', //env var: PGUSER 
   database: 'atlas', //env var: PGDATABASE 
   password: 'password', 
-  host: process.env.NODE_ENV == undefined ? 'geonode' : 'localhost', // Server hosting the postgres database 
+  host: process.env.NODE_ENV === undefined ? process.env.NODE_SERVER || 'geonode' : 'localhost', // Server hosting the postgres database 
   port: 5432, //env var: PGPORT 
   max: 10, // max number of clients in the pool 
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
